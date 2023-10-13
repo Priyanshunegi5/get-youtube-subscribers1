4e756653c12b33369a3635c61dfab97855635ed0
Raman Verma <devramanverma@gmail.com>
2023-10-13 12:57:45 +0530

    Added: db connection and subscribed table model

--- src/models/subscribed.js
+++ src/models/subscribed.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+// Get database connection
+const conn = mongoose.connection
+
+// Get subscribed table
+const table = mongoose.model(
+    "Subscribed", require("./../schemas/subscribed")
+)
