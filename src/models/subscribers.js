f42d49ea4ef9d4d783a0d99790df36f7733c71d8
Raman Verma <devramanverma@gmail.com>
2023-10-13 12:23:40 +0530

    Added: db connection and subscribers table model

--- src/models/subscribers.js
+++ src/models/subscribers.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+// Get database connection
+const conn = mongoose.connection
+
+// Get subscribers table
+const table = mongoose.model(
+    "Subscribers", require("./../schemas/subscribers")
+)
+
+module.exports = {
+    //
+}
