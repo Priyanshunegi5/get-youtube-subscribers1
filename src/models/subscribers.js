3163c9fec9662d8fa0f8359e1f578cb10971952f
Raman Verma <devramanverma@gmail.com>
2023-10-13 12:25:09 +0530

    Added: create function for creating subscriber

--- src/models/subscribers.js
+++ src/models/subscribers.js
@@ -9,5 +9,35 @@ const table = mongoose.model(
 )
 
 module.exports = {
-    //
+    /**
+     * Create a record in the subscribers table.
+     * 
+     * @property {Function} create
+     * 
+     * @param {Object} data Data to be saved in table as record
+     * @param {Function} success Will be called after creation
+     * @param {Function} failure Will be called after failure
+     * @return void
+     */
+    create: function (data, success, failure) {
+        // Create new record in
+        // table with given data.
+        record = new table(data)
+
+        // Save the record in the table
+        record.save((error, data) =>{
+            if (error) {
+                // Call failure
+                return failure(error)
+            }
+
+            try {
+                // Call success
+                return success(data)
+            } catch(caught){
+                // Call failure
+                return failure(error,caught)
+            }
+        });
+    },
 }
