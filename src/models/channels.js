a4cae172596b601ba2b813c74176a0c2184df08d
Raman Verma <devramanverma@gmail.com>
2023-10-13 13:53:55 +0530

    Added: db connection and channels table model

--- src/models/channels.js
+++ src/models/channels.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+// Get database connection
+const conn = mongoose.connection
+
+// Get channels table
+const table = mongoose.model(
+    "Channels", require("../schemas/channels")
+)
