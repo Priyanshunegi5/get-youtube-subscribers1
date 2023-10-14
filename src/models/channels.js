e27459f657da9bfeb0e700d12c2e26eecc446840
Raman Verma <devramanverma@gmail.com>
2023-10-15 00:07:00 +0530

    Added: read function for reading channel

--- src/models/channels.js
+++ src/models/channels.js
@@ -40,4 +40,36 @@ module.exports = {
             }
         });
     },
+
+    /**
+     * Read a record from the channel table.
+     * 
+     * @property {Function} read
+     * 
+     * @param {Object|Null} id Identifier for searching record in table
+     * @param {Function} success Will be called after creation
+     * @param {Function} failure Will be called after failure
+     * @return void
+     */
+    read: function (id, success, failure) {
+        // Find the specifed
+        // record in the table
+        record = table.findOne(id ? id: {})
+
+        // Execute the find instruction in the table
+        record.exec((error, data) =>{
+            if (error) {
+                // Call failure
+                return failure(error)
+            }
+
+            try {
+                // Call success
+                return success(data)
+            } catch(caught){
+                // Call failure
+                return failure(error,caught)
+            }
+        });
+    },
 }
