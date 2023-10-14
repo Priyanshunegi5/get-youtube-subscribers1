6358df929a0c6a6a4e87abb5c9371a6c09c825bc
Raman Verma <devramanverma@gmail.com>
2023-10-14 18:35:39 +0530

    Added: channels create route

--- src/routes/web.js
+++ src/routes/web.js
@@ -16,4 +16,16 @@ router.get("/channels",
     apis.v1.channels.index
 );
 
+/**
+ * The endpoint for creating a channel.
+ * 
+ * @auth none
+ * @method post
+ * @access public
+ * @url protocol://domain.tld/api/v1/channels/create
+ */
+router.post("/channels/create",
+    apis.v1.channels.create
+);
+
 module.exports = { v1: router }
