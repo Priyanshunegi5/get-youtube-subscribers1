cd07856d0ba668a3726ddbc953c5a2f09514c007
Raman Verma <devramanverma@gmail.com>
2023-10-14 12:26:45 +0530

    Added: channels api endpoints collection

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,1 +1,20 @@
-module.exports = { v1: {  } }
+/**
+ * A collection of channels endpoints.
+ * 
+ * An object containing functions for crud operation like:
+ * reading, editing, creating, updating, and removing channels.
+ * 
+ * @type {channel}
+ * @typedef {Object} channel
+ * @property {Function} all - A function endpoint that reads all channels.
+ * @property {Function} read - A function endpoint that reads a channel.
+ * @property {Function} create - A function endpoint that creates a channel.
+ * @property {Function} update - A function endpoint that updates a channel.
+ * @property {Function} delete - A function endpoint that delete a channel.
+ * @property {Function} index - A function endpoint that reads subscriber from channel
+ */
+const channels = {
+    //
+}
+
+module.exports = { v1: { channels } }
