55db7cb548c74f39a2488e8803bfa244c04cda11
Raman Verma <devramanverma@gmail.com>
2023-10-14 15:29:40 +0530

    Added: subscribers all api endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -37,7 +37,7 @@ const channels = {
  * @property {Function} delete - A function endpoint that delete a subscriber.
  */
 const subscribers = {
-    //
+    all:   require("./v1/subscribers/all"),
 }
 
 module.exports = { v1: { channels } }
