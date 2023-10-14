45a16fcda1431381c0319e9aa212ac9bd73f764d
Raman Verma <devramanverma@gmail.com>
2023-10-14 16:35:22 +0530

    Added: channels index route

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,19 @@
 const router = require("express").Router()
 const apis = require("./../api/endpoints")
 
+/////////////////////////////////////////////////////////////////////////////
+// Channel Crude Endpoints version: (V1)
+/////////////////////////////////////////////////////////////////////////////
+/**
+ * The endpoint for reading all subscriber from channel.
+ * 
+ * @auth none
+ * @method post
+ * @access public
+ * @url protocol://domain.tld/api/v1/channels
+ */
+router.get("/channels",
+    apis.v1.channels.index
+);
 
 module.exports = { v1: router }
